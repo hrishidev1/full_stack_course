78cce49345a244261f958cea99c7ab667c93fe4a
hrishidev1 <hrishi03512@gmail.com>
2026-08-11 18:32:23 +0530

    Connect Phonebook frontend to Express backend

--- Part_3/phonebook/index.js
+++ Part_3/phonebook/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 const PORT = 3001
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', (request) => {
   return JSON.stringify(request.body)
