51a467f72434a48a0fd2af55b049a4b85d4f33cb
hrishidev1 <hrishi03512@gmail.com>
2026-08-11 18:15:14 +0530

    Complete Phonebook backend exercises 3.7-3.8

--- Part_3/phonebook/index.js
+++ Part_3/phonebook/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
 const PORT = 3001
 
 app.use(express.json())
+
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
